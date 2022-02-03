1e7b8a55190412d74df9c9e0889f04fdb6be63fc
Edwin Alfaro <edwinalfaro9908@gmail.com>
2022-02-03 13:38:11 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+const app = express();
+
+const api = require('./api/index');
+const connectDB = require('./db/connect');
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api', api);
+
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
